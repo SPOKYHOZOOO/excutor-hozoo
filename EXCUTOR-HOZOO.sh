d5bf967d0c88dee649a120ef0dcefba57ab1f8b6
SPOKYHOZOO <168060492+SPOKYHOZOOO@users.noreply.github.com>
2024-08-26 10:18:49 +0700

    Update EXCUTOR-HOZOO.sh

--- EXCUTOR-HOZOO.sh
+++ EXCUTOR-HOZOO.sh
@@ -1,68 +1,29 @@
 clear
 xdg-open ="https://wa.me/+628999859595?text=.Bang-beli-sc-excutor-bang-hozoo-harga-berapa"
-progreSh() {
-    LR='\033[1;31m'
-    LG='\033[1;32m'
-    LY='\033[1;33m'
-    LC='\033[1;36m'
-    LW='\033[1;37m'
-    NC='\033[0m'
-    if [ "${1}" = "0" ]; then TME=$(date +"%s"); fi
-    SEC=`printf "%04d\n" $(($(date +"%s")-${TME}))`; SEC="$SEC sec"
-    PRC=`printf "%.0f" ${1}`
-    SHW=`printf "%3d\n" ${PRC}`
-    LNE=`printf "%.0f" $((${PRC}/2))`
-    LRR=`printf "%.0f" $((${PRC}/2-12))`; if [ ${LRR} -le 0 ]; then LRR=0; fi;
-    LYY=`printf "%.0f" $((${PRC}/2-24))`; if [ ${LYY} -le 0 ]; then LYY=0; fi;
-    LCC=`printf "%.0f" $((${PRC}/2-36))`; if [ ${LCC} -le 0 ]; then LCC=0; fi;
-    LGG=`printf "%.0f" $((${PRC}/2-48))`; if [ ${LGG} -le 0 ]; then LGG=0; fi;
-    LRR_=""
-    LYY_=""
-    LCC_=""
-    LGG_=""
-    for ((i=1;i<=13;i++))
-    do
-    	DOTS=""; for ((ii=${i};ii<13;ii++)); do DOTS="${DOTS}."; done
-    	if [ ${i} -le ${LNE} ]; then LRR_="${LRR_}#"; else LRR_="${LRR_}."; fi
-    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${DOTS}${LY}............${LC}............${LG}............ ${SHW}%${NC}\r"
-    	if [ ${LNE} -ge 1 ]; then sleep .09; fi
-    done
-    for ((i=14;i<=30;i++))
-    do
-    	DOTS=""; for ((ii=${i};ii<30;ii++)); do DOTS="${DOTS}."; done
-    	if [ ${i} -le ${LNE} ]; then LYY_="${LYY_}#"; else LYY_="${LYY_}."; fi
-    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${DOTS}${LC}............${LG}............ ${SHW}%${NC}\r"
-    	if [ ${LNE} -ge 19 ]; then sleep .09; fi
-    done
-    for ((i=26;i<=37;i++))
-    do
-    	DOTS=""; for ((ii=${i};ii<40;ii++)); do DOTS="${DOTS}."; done
-    	if [ ${i} -le ${LNE} ]; then LCC_="${LCC_}#"; else LCC_="${LCC_}."; fi
-    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${DOTS}${LG}............ ${SHW}%${NC}\r"
-    	if [ ${LNE} -ge 30 ]; then sleep .09; fi
-    done
-    for ((i=38;i<=60;i++))
-    do
-    	DOTS=""; for ((ii=${i};ii<49;ii++)); do DOTS="${DOTS}."; done
-    	if [ ${i} -le ${LNE} ]; then LGG_="${LGG_}#"; else LGG_="${LGG_}."; fi
-    	echo -ne "  ${LW}${SEC}  ${LR}${LRR_}${LY}${LYY_}${LC}${LCC_}${LG}${LGG_}${DOTS} ${SHW}%${NC}\r"
-    	if [ ${LNE} -ge 38 ]; then sleep .05; fi
-    done
-}
+sleep 0.4
+## Install Cloudflared
+install_cloudflared() {
+	if [[ -e ".server/cloudflared" ]]; then
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${GREEN} Cloudflared already installed."
+	else
+		echo -e "\n${GREEN}[${WHITE}+${GREEN}]${CYAN} Installing Cloudflared..."${WHITE}
+		arch=`uname -m`
+		if [[ ("$arch" == *'arm'*) || ("$arch" == *'Android'*) ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm' 'cloudflared'
+		elif [[ "$arch" == *'aarch64'* ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-arm64' 'cloudflared'
+		elif [[ "$arch" == *'x86_64'* ]]; then
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64' 'cloudflared'
+		else
+			download 'https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-386' 'cloudflared'
+		fi
+	fi
+} 
+sleep 0.4
+printf '{5951232585}'
+echo " id user "
+sleep 0.4
 
-printf "\n\n\n\n\n\n\n\n\n\n"
-progreSh 0
-progreSh 10
-progreSh 20
-progreSh 30
-progreSh 40
-progreSh 50
-progreSh 60
-progreSh 70
-progreSh 80
-progreSh 90
-progreSh 100
-printf "\n\n\n\n\n\n\n\n\n\n"
 echo -e "YOUTUBE: SPOKYHOZOO"
 echo -e "TIKTOK :SPOKYHOZOO02"
 echo -e "TEAM: SYSTEM UMBRELLA DRAK999"
